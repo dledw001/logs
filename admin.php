bb7f213692609a897c4e2542b6e6138555b1137e
Dan Ledwith <dledw001@odu.edu>
2025-03-04 17:36:02 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -34,7 +34,7 @@ $logs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Admin Panel - Logger App</title>
+  <title>Logs - Admin</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="icon" href="assets/favicon.png" type="image/x-icon">
   <style>
