492568cf563ff75abe60f711eb7ea6c1d41c6e52
Dan Ledwith <dledw001@odu.edu>
2025-03-04 07:02:31 +0000

    Update index.php

--- index.php
+++ index.php
@@ -26,14 +26,14 @@ if (isset($_SESSION['username'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Logger App - Login</title>
+  <title>Logs</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="icon" href="assets/favicon.png" type="image/x-icon">
 </head>
 <body>
   <div class="container">
     <img src="assets/logs_color_256.png" alt="Site Logo" class="logo">
-    <h1>Logs</h1>
+    <h1>Login to Logs</h1>
     <form action="login.php" method="post">
       <label for="username">Username:</label>
       <input type="text" id="username" name="username" required placeholder="Enter your username">
