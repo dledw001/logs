d6e5c55d8379946d9d54640f713fd834c3bc98d8
Dan Ledwith <dledw001@odu.edu>
2025-03-04 07:03:10 +0000

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['username'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Logs</title>
+  <title>Logs - Login</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="icon" href="assets/favicon.png" type="image/x-icon">
 </head>
