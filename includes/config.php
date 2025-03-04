1d526c34ab6902a57e752677760674fadaab9057
Dan Ledwith <dledw001@odu.edu>
2025-03-04 07:17:00 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 // Database configuration – adjust these values to match your setup.
 $dbHost = "localhost";
-$dbName = "logger_app";
-$dbUser = "your_db_username";
-$dbPass = "your_db_password";
+$dbName = "logs";
+$dbUser = "dledwith";
+$dbPass = "password";
 ?>
