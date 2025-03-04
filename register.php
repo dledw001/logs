492713b45232d9ec84c4e587973a505189017421
Dan Ledwith <dledw001@odu.edu>
2025-03-04 07:02:58 +0000

    Update register.php

--- register.php
+++ register.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Logger App - Register</title>
+  <title>Logs - Register</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="icon" href="assets/favicon.png" type="image/x-icon">
 </head>
