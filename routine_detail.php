803b79c93134239607823c9ebc00191f238c414d
Dan Ledwith <dledw001@odu.edu>
2025-03-06 12:47:16 -0500

    update routine_detail.php

--- routine_detail.php
+++ routine_detail.php
@@ -119,7 +119,7 @@ foreach ($elements as $element) {
     <?php endif; ?>
 
     <p><?php echo $routine_id; ?></p>
-    <p><?php echo $log_entries[0]; ?></p>
+    <p><?php echo $log_entries[1]; ?></p>
 
     <h2>Add New Element</h2>
     <form action="routine_detail.php?routine_id=<?php echo $routine_id; ?>" method="post">
