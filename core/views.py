4df176df9a74dbe38352c11b30e6e4f612e60226
Dan Ledwith <dledw001@odu.edu>
2025-09-27 23:42:49 -0400

    initial commit

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+def home(request):
+    return render(request, 'core/home.html')
+
+def root(request):
+    if request.user.is_authenticated:
+        return redirect("dashboard")
+    return redirect("account_login")
+
+@login_required
+def dashboard(request):
+    return render(request, "core/dashboard.html")
